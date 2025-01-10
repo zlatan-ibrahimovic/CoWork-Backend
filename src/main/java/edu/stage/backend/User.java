b102982a8aaf64e7b61032cea064db642663b0a7
souliyan <souliyan33@gmail.com>
2025-01-10 14:14:20 +0100

    Restoration du projet en cours après des erreurs aux niveaux des branches

---
+++ src/main/java/edu/stage/backend/User.java
@@ -0,0 +1,54 @@
+package edu.stage.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Email
+    @NotBlank(message = "L'email ne peut être vide")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe ne peut pas être vide")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    private String password;
+
+    public Long getid() {
+        return id;
+    }
+
+    public void setid(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Email invalide !");
+        }
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
