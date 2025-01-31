91801acbd8d964c2991993f96e069d1a38698b3f
souliyan <souliyan33@gmail.com>
2025-01-31 15:59:56 +0100

    fix/US4-backend-auth

--- src/main/java/edu/stage/backend/controller/UserController.java
+++ src/main/java/edu/stage/backend/controller/UserController.java
@@ -24,9 +24,8 @@ public class UserController {
         System.out.println("Email reçu : " + user.getEmail());
 
         
-        if (userRepository.findByEmail(user.getEmail()) != null) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             System.out.println("Conflit : l'utilisateur avec l'email existe déjà.");
-
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
 
